f9b444a67cdb87a5ebcb1b66de99f620daa2046b
sean_park <19980819@naver.com>
2020-05-20 20:29:01 +0900

    done app.js setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import '../style/App.css';
+import Login from './Login';
+import Login from './SignUp';
+import Login from './Main';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+    state = {
       logged: false
     };
   }
+  
   render() {
     return (
       <div className="App">
-        
+        <Switch>
+          <Route path="/login" render={() => <Login logged={this.state.logged} />} />
+          <Route
+            exact
+            path="/signUp"
+            render={() => <Signup logged={this.state.logged} />}
+          />
+          <Route
+            exact
+            path="/main"
+            render={() => <Main logged={this.state.logged}/>}
+          />
+          <Route
+            path="/"
+            render={() => {
+              if (this.state.logged) {
+                return <Redirect to="/main" />;
+              } else {
+                return <Redirect to="/login" />;
+              }
+            }}
+          />
+        </Switch>
       </div>
     );
   }
@@ -20,7 +47,7 @@ class App extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    logged: state.logged
+    logged: state.userReducer.logged
   };
 };
 
