16ec537758902dbb697e0eb6fededa9f71d7fed8
sean_park <19980819@naver.com>
2020-05-20 20:13:40 +0900

    branch를 햇갈려서 수정해서 다시 커밋합니다

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,6 @@ import React from 'react';
 import '../style/App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      logged: false
-    };
-  }
   render() {
     return (
       <div className="App">
@@ -18,10 +11,4 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    logged: state.logged
-  };
-};
-
-export default connect(mapStateToProps)(App);
+export default App;
