bfb40da624b08d911a4387bb25243bef92dfd1bd
sean_park <19980819@naver.com>
2020-05-21 17:57:35 +0900

    done fixed login/signup to satisfied for eslint

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import axios from 'axios';
-import { setLogged, setUserData } from "../actions";
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { history as historyPropTypes } from 'history-prop-types';
+import { setLogged, setUserData } from '../actions';
 
 class Login extends React.Component {
   constructor(props) {
@@ -10,63 +12,92 @@ class Login extends React.Component {
 
     this.state = {
       email: '',
-      password: ''
+      password: '',
     };
     this.handleInputValue = this.handleInputValue.bind(this);
     this.submit = this.submit.bind(this);
-  };
+  }
 
-  handleInputValue = key => e => {
+  handleInputValue = (key) => (e) => {
     this.setState({ [key]: e.target.value });
   };
 
   submit = () => {
-    axios.post('http://13.125.27.141:5000/signin', {
-          email: this.state.email,
-          password: this.state.password
+    const {
+      history,
+      loggedDispatch,
+      userDataDispatch,
+    } = this.props;
+    const {
+      email,
+      password,
+    } = this.state;
+    axios.post('http://15.164.226.124:5000/signin', {
+      email,
+      password,
+    })
+      .then((res) => {
+        if (res.data === 'NO DATA') {
+          alert('등록된 계정이 없습니다');
+        } else {
+          loggedDispatch();
+          userDataDispatch(res);
+        }
+        // this.props.dispatch(setLogged(true));
+        // this.props.dispatch(setUserData(res));
       })
-      .then(res => {
-        this.props.dispatch(setLogged(true));
-        this.props.dispatch(setUserData(res));
-      })
-      .then(() => this.props.history.push('/'))
-      .catch(error => {
+      .then(() => history.push('/'))
+      .catch((error) => {
         console.log(error);
       });
   }
 
   toSignUp = () => {
-    this.props.history.push('/signup')
+    const { history } = this.props;
+    history.push('/signup');
   }
 
-  render(){
-      return(
-    <div className="Login">
-     <div>
-       <img src="#" alt="" />
-     </div>
-     <div className="email">
-       이메일 :
-       <input className="emailInput" size="30" type="email" onChange={this.handleInputValue('email')} required />
-     </div>
-     <div className="password">
-       비밀번호 :
-       <input className="passwordInput" size="30" type="password" onChange={this.handleInputValue('password')} required />
-     </div>
-     <div className="loginButton">
-       <button className="signInSubmitButton" type="submit" onClick={this.submit}>로그인</button>
-       <button className="signUpButton" type="button" onClick={this.toSignUp}>회원가입</button>
-     </div>
-   </div>
-   )
+  render() {
+    return (
+      <div className="Login">
+        <div>
+          <img src="#" alt="" />
+        </div>
+        <div className="email">
+          이메일 :
+          <input className="emailInput" size="30" type="email" onChange={this.handleInputValue('email')} required />
+        </div>
+        <div className="password">
+          비밀번호 :
+          <input className="passwordInput" size="30" type="password" onChange={this.handleInputValue('password')} required />
+        </div>
+        <div className="loginButton">
+          <button className="signInSubmitButton" type="submit" onClick={this.submit}>로그인</button>
+          <button className="signUpButton" type="button" onClick={this.toSignUp}>회원가입</button>
+        </div>
+      </div>
+    );
   }
+}
+
+Login.propTypes = {
+  history: PropTypes.shape(historyPropTypes),
+  loggedDispatch: PropTypes.func.isRequired,
+  userDataDispatch: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => {
-    return {
-      logged: state.user.logged,
-      loggedUser: state.user.loggedUser,
-    };
-  };
+Login.defaultProps = {
+  history: [],
+};
 
-export default connect(mapStateToProps)(withRouter(Login));
+const mapStateToProps = (state) => ({
+  logged: state.user.logged,
+  loggedUser: state.user.loggedUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loggedDispatch: () => dispatch(setLogged(true)),
+  userDataDispatch: (res) => dispatch(setUserData(res)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Login));
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-// import { setLogged, setUserData } from "../actions";
-// import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { history as historyPropTypes } from 'history-prop-types';
 import { withRouter } from 'react-router-dom';
 
 class SignUp extends React.Component {
@@ -12,66 +12,79 @@ class SignUp extends React.Component {
       email: '',
       name: '',
       password: '',
-      mobile: ''
+      mobile: '',
     };
     this.handleInputValue = this.handleInputValue.bind(this);
     this.submitSignUp = this.submitSignUp.bind(this);
-  };
+  }
 
-  handleInputValue = key => e => {
+  handleInputValue = (key) => (e) => {
     this.setState({ [key]: e.target.value });
   };
 
   submitSignUp = () => {
-    axios.post('http://13.125.27.141:5000/signup', {
-          email: this.state.email,
-          password: this.state.password,
-          name: this.state.name,
-          mobile: this.state.mobile
-      })
-      .then(res => {
-          console.log(res)
-        if(res.data === 'ok'){
-            alert('회원가입이 완료되었습니다');
-            this.props.history.push('/login');
-        } else if (res.data === 'duplicate'){
-            alert('이미 가입한 계정입니다');
+    const {
+      email,
+      password,
+      name,
+      mobile,
+    } = this.state;
+    axios.post('http://15.164.226.124:5000/signup', {
+      email,
+      password,
+      name,
+      mobile,
+    })
+      .then((res) => {
+        const { history } = this.props;
+        if (res.data === 'ok') {
+          alert('회원가입이 완료되었습니다');
+          history.push('/login');
+        } else if (res.data === 'duplicate') {
+          alert('이미 가입한 계정입니다');
         }
       })
-    //   .then(() => this.props.history.push('/login'))
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }
 
-  render(){
-      return(
-    <div className="Login">
-     <div>
-       <img src="#" alt="" />
-     </div>
-     <div className="email">
-       이메일 :
-       <input className="emailInput" size="30" type="email" onChange={this.handleInputValue('email')} required />
-     </div>
-     <div className="password">
-       비밀번호 :
-       <input className="passwordInput" size="30" type="password" onChange={this.handleInputValue('password')} required />
-     </div>
-     <div className="name">
-       이름 :
-       <input className="nameInput" size="30" type="text" onChange={this.handleInputValue('name')} required />
-     </div>
-     <div className="mobile">
-       전화번호 :
-       <input className="mobileInput" size="30" type="mobile" onChange={this.handleInputValue('mobile')} required />
-     </div>
-     <div className="signUpButton">
-       <button className="signUpSubmitButton" type="button" onClick={this.submitSignUp}>완료</button>
-     </div>
-   </div>
-   )
+  render() {
+    return (
+      <div className="Login">
+        <div>
+          <img src="#" alt="" />
+        </div>
+        <div className="email">
+          이메일 :
+          <input className="emailInput" size="30" type="email" onChange={this.handleInputValue('email')} required />
+        </div>
+        <div className="password">
+          비밀번호 :
+          <input className="passwordInput" size="30" type="password" onChange={this.handleInputValue('password')} required />
+        </div>
+        <div className="name">
+          이름 :
+          <input className="nameInput" size="30" type="text" onChange={this.handleInputValue('name')} required />
+        </div>
+        <div className="mobile">
+          전화번호 :
+          <input className="mobileInput" size="30" type="mobile" onChange={this.handleInputValue('mobile')} required />
+        </div>
+        <div className="signUpButton">
+          <button className="signUpSubmitButton" type="button" onClick={this.submitSignUp}>완료</button>
+        </div>
+      </div>
+    );
   }
+}
+
+SignUp.propTypes = {
+  history: PropTypes.shape(historyPropTypes),
+};
+
+SignUp.defaultProps = {
+  history: [],
 };
 
 export default withRouter(SignUp);
