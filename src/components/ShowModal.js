38c3eb1c6b2161051bb64768ed7a15dde45b2166
TheJang <wkddnjs7310@naver.com>
2020-05-22 17:00:29 +0900

    showModal.js modify

--- src/components/ShowModal.js
+++ src/components/ShowModal.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactModal from 'react-modal'
 import '../style/ShowModal.css';
 
-// ReactModal.setAppElement('#root');
+ReactModal.setAppElement('#root');
 
 class ShowModal extends React.Component {
   constructor (props) {
@@ -36,63 +36,125 @@ class ShowModal extends React.Component {
               
             >
               <div>휴가 신청</div>
-                <div className = "modalVacationStart"> 시작일
+                <div className = 'modalVacationStart'> 시작일
                   <select > 
-                    <option value="2020">2020</option>
-                    <option value="2020">2021</option>
-                    <option value="2020">2022</option>
+                    <option value='2020'>2020</option>
+                    <option value='2020'>2021</option>
+                    <option value='2020'>2022</option>
                   </select>연도
                   
                    <select>
-                     <option value="1">1</option>
-                     <option value="2">2</option>
-                     <option value="3">3</option>
-                     <option value="4">4</option>
-                     <option value="5">5</option>
-                     <option value="6">6</option>
-                     <option value="7">7</option>
-                     <option value="8">8</option>
-                     <option value="9">9</option>
-                     <option value="10">10</option>
-                     <option value="11">11</option>
-                     <option value="12">12</option>
+                     <option value='1'>1</option>
+                     <option value='2'>2</option>
+                     <option value='3'>3</option>
+                     <option value='4'>4</option>
+                     <option value='5'>5</option>
+                     <option value='6'>6</option>
+                     <option value='7'>7</option>
+                     <option value='8'>8</option>
+                     <option value='9'>9</option>
+                     <option value='10'>10</option>
+                     <option value='11'>11</option>
+                     <option value='12'>12</option>
                   </select>월
                   
                   <select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                    <option value='5'>5</option>
+                    <option value='6'>6</option>
+                    <option value='7'>7</option>
+                    <option value='8'>8</option>
+                    <option value='9'>9</option>
+                    <option value='10'>10</option>
+                    <option value='11'>11</option>
+                    <option value='12'>12</option>
+                    <option value='13'>13</option>
+                    <option value='14'>14</option>
+                    <option value='15'>15</option>
+                    <option value='16'>16</option>
+                    <option value='17'>17</option>
+                    <option value='18'>18</option>
+                    <option value='19'>19</option>
+                    <option value='20'>20</option>
+                    <option value='21'>21</option>
+                    <option value='22'>22</option>
+                    <option value='23'>23</option>
+                    <option value='24'>24</option>
+                    <option value='25'>25</option>
+                    <option value='26'>26</option>
+                    <option value='27'>27</option>
+                    <option value='28'>28</option>
+                    <option value='29'>29</option>
+                    <option value='30'>30</option>
+                    <option value='31'>31</option>
                 </select>일
 
-                <div className = "reason"> 사유 
+                <div>
+                종료일
+                  <select>
+                      <option value='2020'>2020</option>
+                      <option value='2020'>2021</option>
+                      <option value='2020'>2022</option>
+                  </select>연도
+
+                  <select>
+                     <option value='1'>1</option>
+                     <option value='2'>2</option>
+                     <option value='3'>3</option>
+                     <option value='4'>4</option>
+                     <option value='5'>5</option>
+                     <option value='6'>6</option>
+                     <option value='7'>7</option>
+                     <option value='8'>8</option>
+                     <option value='9'>9</option>
+                     <option value='10'>10</option>
+                     <option value='11'>11</option>
+                     <option value='12'>12</option>
+                  </select>월
+
+                  <select>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                    <option value='5'>5</option>
+                    <option value='6'>6</option>
+                    <option value='7'>7</option>
+                    <option value='8'>8</option>
+                    <option value='9'>9</option>
+                    <option value='10'>10</option>
+                    <option value='11'>11</option>
+                    <option value='12'>12</option>
+                    <option value='13'>13</option>
+                    <option value='14'>14</option>
+                    <option value='15'>15</option>
+                    <option value='16'>16</option>
+                    <option value='17'>17</option>
+                    <option value='18'>18</option>
+                    <option value='19'>19</option>
+                    <option value='20'>20</option>
+                    <option value='21'>21</option>
+                    <option value='22'>22</option>
+                    <option value='23'>23</option>
+                    <option value='24'>24</option>
+                    <option value='25'>25</option>
+                    <option value='26'>26</option>
+                    <option value='27'>27</option>
+                    <option value='28'>28</option>
+                    <option value='29'>29</option>
+                    <option value='30'>30</option>
+                    <option value='31'>31</option>
+                </select>일
+                </div>
+                  
+               
+
+                
+
+                <div className = 'reason'> 사유 
                     <select >
                     <option > 병가 </option>
                     <option > 개인 사정  </option>
@@ -100,7 +162,7 @@ class ShowModal extends React.Component {
                     </select>연도
                 </div>
               </div>
-              <button onClick={this.handleCloseModal}>휴가 신청</button>
+              <button id ='modal-button' onClick={this.handleCloseModal}>휴가 신청</button>
             </ReactModal>
           </div>
         );
