48cb1f247911bc21d8db86fca2d355fc9388bf4a
sean_park <19980819@naver.com>
2020-05-25 06:24:41 +0900

    ing set Auth funcion

--- src/components/EmployeeEntry.js
+++ src/components/EmployeeEntry.js
@@ -6,6 +6,32 @@ import PropTypes from 'prop-types';
 
 const EmployeeEntry = (props) => {
   const { employee } = props;
+
+  const updateAuthToManager = () => {
+    const data = {
+      type: 'setAuth',
+      auth: 'manager',
+      email: employee.email,
+    };
+    fetch('http://15.164.226.124:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if (res.email === employee.email) {
+          alert('권한이 변경되었습니다');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="EmployeeEntry">
       <div>{employee.name}</div>
@@ -16,7 +42,7 @@ const EmployeeEntry = (props) => {
       <div>{11 - employee.totalVacation}</div>
       <div>{employee.totalVacation}</div>
       <div>
-        <button type="button">관리자지정</button>
+        <button type="button" onClick={updateAuthToManager}>관리자지정</button>
       </div>
     </div>
   );
@@ -29,7 +55,7 @@ EmployeeEntry.propTypes = {
     totalVacation: PropTypes.number,
   }).isRequired,
 //   history: PropTypes.shape(historyPropTypes)
-//   searchDispatch: PropTypes.func.isRequired,
+//   authDispatch: PropTypes.func.isRequired,
 };
 
 
